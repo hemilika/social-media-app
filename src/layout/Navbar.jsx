242fc032d5f249655791fdda82c03fcaa8d6ce8d
hemilika <144246364+hemilika@users.noreply.github.com>
2024-03-06 10:57:46 +0100

    test

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Box from "@mui/material/Box";
+import { Stack } from "@mui/material";
+const Navbar = () => {
+  return (
+    <Box sx={{ background: "red" }}>
+      <Stack>Hello</Stack>
+    </Box>
+  );
+};
+
+export default Navbar;
