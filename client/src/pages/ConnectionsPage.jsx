52b3182a059b25ebbfcbca100267502645ed9b68
Hemi Lika <hemilika11@gmail.com>
2024-04-03 19:23:16 +0200

    added login page

--- client/src/pages/ConnectionsPage.jsx
+++ client/src/pages/ConnectionsPage.jsx
@@ -1,7 +1,14 @@
+import { Grid } from "@mui/material";
 import YourConnections from "../components/connections/YourConnections";
 
 const ConnectionsPage = () => {
-  return <YourConnections />;
+  return (
+    <Grid container>
+      <Grid item>
+        <YourConnections />;
+      </Grid>
+    </Grid>
+  );
 };
 
 export default ConnectionsPage;
